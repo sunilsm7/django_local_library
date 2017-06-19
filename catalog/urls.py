074153e102004b47a87284146e7a9c72d6066ed4
Sunil <sunil9766@gmail.com>
2017-06-19 17:58:35 +0530

    book renew form added

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,15 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^books/$', views.BookListView.as_view(), name='books'),
-	url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-	url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
-	url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
-	url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-	url(r'^borrowed/$', views.LoanBooksAllListView.as_view(), name ='all-borrowed'),
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^borrowed/$', views.LoanBooksAllListView.as_view(), name='all-borrowed'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
-
-
-
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.contrib.auth.decorators import login_required
@@ -6,6 +6,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import datetime
+from .forms import RenewBookForm
+
 
 # Create your views here.
 
@@ -76,19 +81,42 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
 
 class LoanBooksAllListView(PermissionRequiredMixin, generic.ListView):
-	"""
-	Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission.
-	"""
-	model = BookInstance
-	context_object_name = 'bookinstance_list'
-	permission_required = 'catalog.can_mark_returned'
-	template_name = 'catalog/bookinstance_list_borrowed_all.html'
-	paginate_by = 10
+    """
+    Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission.
+    """
+    model = BookInstance
+    context_object_name = 'bookinstance_list'
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
 
-	def get_queryset(self):
-		return BookInstance.objects.filter(status__exact = 'o').order_by('due_back')
-
-
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
 
 
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk = pk)
 
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request(binding)
+        form = RenewBookForm(request.POST)
+
+        # check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+           
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    # If this is a GET create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial= {'renewal_date':proposed_renewal_date,})
+
+    return render(request, 'catalog/book_renew_librarian.html', {'form' : form, 'book_inst' : book_inst})
