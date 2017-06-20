67a8518eb3aa536d8a44a397377938623352bae4
Sunil <sunil9766@gmail.com>
2017-06-20 11:38:40 +0530

    author and book model crud done

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,4 +11,16 @@ urlpatterns = [
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     url(r'^borrowed/$', views.LoanBooksAllListView.as_view(), name='all-borrowed'),
     url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+
+    # Author CRUD
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+
+    # Book CRUD
+	url(r'^books/create/$', views.BookCreate.as_view(), name='book_create'),
+    url(r'^book/(?P<pk>\d+)/update/$', views.BookUpdate.as_view(), name='book_update'),
+    url(r'^book/(?P<pk>\d+)/delete/$', views.BookDelete.as_view(), name='book_delete'),
+
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -10,7 +10,8 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 import datetime
 from .forms import RenewBookForm, RenewBookModelForm
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -120,3 +121,41 @@ def renew_book_librarian(request, pk):
         form = RenewBookForm(initial= {'renewal_date':proposed_renewal_date,})
 
     return render(request, 'catalog/book_renew_librarian.html', {'form' : form, 'book_inst' : book_inst})
+
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '12/10/2019',}
+    permission_required = 'catalog.can_mark_returned'
+
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    permission_required = 'catalog.can_mark_returned'
+
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.can_mark_returned'
+
+
+class BookCreate(PermissionRequiredMixin, CreateView):
+    model = Book
+    fields = '__all__'
+    permission_required = 'catalog.can_mark_returned'
+
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    model = Book
+    #fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    fields = '__all__'
+    permission_required = 'catalog.can_mark_returned'
+
+
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
+    permission_required = 'catalog.can_mark_returned'
